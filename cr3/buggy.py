af172a4034f6eb32451bc22e217f917192b80b3f
summersphinx <summersphinx@duck.com>
2023-03-28 15:37:03 -0600

    Update bug reporting

--- cr3/buggy.py
+++ cr3/buggy.py
@@ -7,46 +7,48 @@ import traceback
 
 project_name = 'CodeRandom3'
 
-if not os.path.exists(f'{os.getenv("LOCALAPPDATA")}/XPlus Games/{project_name}/logs'):
-    os.makedirs(f'{os.getenv("LOCALAPPDATA")}/XPlus Games/{project_name}/logs', True)
+if False:
+
+    if not os.path.exists(f'{os.getenv("LOCALAPPDATA")}/XPlus Games/{project_name}/logs'):
+        os.makedirs(f'{os.getenv("LOCALAPPDATA")}/XPlus Games/{project_name}/logs', True)
 
 
-t = str(datetime.datetime.now()).replace(':', '_')
-log_name = f'{os.getenv("LOCALAPPDATA")}/XPlus Games/{project_name}/logs/{t} - log.log'
-print(log_name)
+    t = str(datetime.datetime.now()).replace(':', '_')
+    log_name = f'{os.getenv("LOCALAPPDATA")}/XPlus Games/{project_name}/logs/{t} - log.log'
+    print(log_name)
 
-with open(log_name, 'x') as fh:
-    pass
-log = logzero.setup_logger(name='Logger', logfile=log_name)
+    with open(log_name, 'x') as fh:
+        pass
+    log = logzero.setup_logger(name='Logger', logfile=log_name)
 
-log.info('Buggy Loaded')
+    log.info('Buggy Loaded')
 
 
-class Splat:
+    class Splat:
 
-    def __init__(self, exception, email, log, user):
-        bugsplat = BugSplat(f'{project_name}', f'{project_name}', '0.1.1')
-        if log:
-            # noinspection PyTypeChecker
-            bugsplat.post(exception, app_key='app_key', email=email, additional_file_paths=[log_name], user=user)
-        else:
-            bugsplat.post(exception, app_key='app_key', email=email, user=user)
+        def __init__(self, exception, email, log, user):
+            bugsplat = BugSplat(f'{project_name}', f'{project_name}', '0.1.1')
+            if log:
+                # noinspection PyTypeChecker
+                bugsplat.post(exception, app_key='app_key', email=email, additional_file_paths=[log_name], user=user)
+            else:
+                bugsplat.post(exception, app_key='app_key', email=email, user=user)
 
-    def report(self, exception):
-        log.error(traceback.format_tb(exception.__traceback__))
-        layout = [
-            [sg.Image(f'{os.getenv("LOCALAPPDATA")}/XPlus Games/{project_name}/bugsplat-brand.png', s=(50, 50)), sg.Text('An error has occurred. If you wish to help this game be better, then feel free to submit the bug below.')],
-            [sg.Frame('Error:', [
-                [sg.Multiline('', disabled=True, k='error', s=(70, 15))]
-            ])],
-            [sg.Frame('Optional', [[sg.Text('Name:'), sg.Input('', k='name'), sg.Text('Email:'), sg.Input('', tooltip='Your email address. This is if I need to reach you.', k='email')]])],
-            [sg.Checkbox('Send bug report', k='send'), sg.Checkbox('Send log File', k='log'), sg.Frame('', [[sg.Button('Finish')]], expand_x=True, element_justification='right', border_width=0)]
-        ]
+        def report(self, exception):
+            log.error(traceback.format_tb(exception.__traceback__))
+            layout = [
+                [sg.Image(f'{os.getenv("LOCALAPPDATA")}/XPlus Games/{project_name}/bugsplat-brand.png', s=(50, 50)), sg.Text('An error has occurred. If you wish to help this game be better, then feel free to submit the bug below.')],
+                [sg.Frame('Error:', [
+                    [sg.Multiline('', disabled=True, k='error', s=(70, 15))]
+                ])],
+                [sg.Frame('Optional', [[sg.Text('Name:'), sg.Input('', k='name'), sg.Text('Email:'), sg.Input('', tooltip='Your email address. This is if I need to reach you.', k='email')]])],
+                [sg.Checkbox('Send bug report', k='send'), sg.Checkbox('Send log File', k='log'), sg.Frame('', [[sg.Button('Finish')]], expand_x=True, element_justification='right', border_width=0)]
+            ]
 
-        erwn = sg.Window('Uh Oh!', layout, finalize=True, icon=f'{os.getenv("LOCALAPPDATA")}/XPlus Games/{project_name}/bugsplat.ico')
-        erwn['error'].update(traceback.format_tb(exception.__traceback__))
-        event, values = erwn.read(close=True)
-        if values is None:
-            pass
-        elif values['send']:
-            Splat(exception, values['email'], values['log'], values['name'])
+            erwn = sg.Window('Uh Oh!', layout, finalize=True, icon=f'{os.getenv("LOCALAPPDATA")}/XPlus Games/{project_name}/bugsplat.ico')
+            erwn['error'].update(traceback.format_tb(exception.__traceback__))
+            event, values = erwn.read(close=True)
+            if values is None:
+                pass
+            elif values['send']:
+                Splat(exception, values['email'], values['log'], values['name'])
