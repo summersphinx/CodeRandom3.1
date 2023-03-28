3e4834087ea9d5725c99395afc89074c9b1d6a1f
summersphinx <summersphinx@duck.com>
2023-03-28 15:38:37 -0600

    Add additional Settings, add credits

--- cr3/__init__.py
+++ cr3/__init__.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import os
 import PySimpleGUI as sg
-from cr3.buggy import log
+from cr3.tab import tab_group
+# from cr3.buggy import log
 
 path = os.getenv('LOCALAPPDATA') + '/XPlus Games/CodeRandom3/'
 
@@ -9,16 +10,24 @@ class Settings:
 
     def __init__(self):
         settings_dict = pd.read_csv(path + 'settings.csv', index_col=0, header=None).to_dict()[1]
-        log.info(settings_dict)
+        print(settings_dict)
         self.theme = settings_dict['theme']
         self.darkness = settings_dict['dark mode'] == 'True'
+        self.font_a = settings_dict['font a']
+        self.font_b = settings_dict['font b']
+        self.music_vol = float(settings_dict['music vol'])
+        self.music = settings_dict['music'] == 'True'
+        self.voice_vol = float(settings_dict['voice vol'])
+        self.voice = settings_dict['voice']
+        self.sfx_vol = float(settings_dict['sfx vol'])
+        self.sfx = settings_dict['sfx'] == 'True'
 
     def save(self, to_change,  new_value):
         settings_dict = pd.read_csv(path + 'settings.csv', index_col=0, header=None).to_dict()[1]
         settings_dict[to_change] = new_value
-        log.debug(type(settings_dict))
+        print(type(settings_dict))
         test = pd.DataFrame.from_dict(settings_dict, orient='index').to_csv(path + 'settings.csv')
-        log.info(test)
+        print(test)
 
 # Credit Colors & Fonts for the Color Pallets used
 class Colors:
@@ -51,7 +60,7 @@ class Colors:
         color_tree = Colors.GetColors()
         if dark_mode:
             color_tree[colors].reverse()
-        log.info(colors)
+        print(colors)
 
         theme = {
             'BACKGROUND': color_tree[colors][0],
@@ -75,6 +84,7 @@ def Layouts():
     menu = [
         [sg.Button('Back', k='back', expand_x=True, visible=False)],
         [sg.Button('New Game', k='new', expand_x=True)],
+        [sg.Button('Tab', k='tabgroup', expand_x=True)],
         [sg.Button('Continue', k='cont', expand_x=True)],
         [sg.Button('Docs', k='docs', expand_x=True)],
         [sg.Button('Settings', k='settings', expand_x=True)],
@@ -98,10 +108,41 @@ def Layouts():
         [sg.Checkbox('Dark Mode', k='dark mode', default=sett.darkness)]
     ]
 
+    installed_fonts = sg.Text.fonts_installed_list()
+    installed_fonts.extend(['Nunito', 'Source Sans Pro Light'])
+    installed_fonts.sort()
+
+    settings_fonts = [
+        [sg.Text('Custom fonts provided by Google Fonts')],
+        [sg.InputCombo(installed_fonts, expand_x=True, pad=(50, 10), default_value=sett.font_a, k='font a')],
+        [sg.InputCombo(installed_fonts, expand_x=True, pad=(50, 10), default_value=sett.font_b, k='font b')]
+    ]
+
+    settings_music = [
+        [sg.Slider([0, 1], default_value=sett.music_vol, orientation='h', resolution=0.01, expand_x=True, pad = (50, 10), k='music vol')],
+        [sg.Checkbox('Mute', k='music', default=sett.music)]
+    ]
+
+    settings_sfx = [
+        [sg.Slider([0, 1], default_value=sett.sfx_vol, orientation='h', resolution=0.01, expand_x=True, pad=(50, 10),
+                   k='sfx vol')],
+        [sg.Checkbox('Mute', k='sfx', default=sett.sfx)]
+    ]
+
+    settings_voice = [
+        [sg.InputOptionMenu(['Robot', 'Off'], expand_x=True, pad=(30, 10), default_value=sett.voice,
+                            k='voice', disabled=True)],
+        [sg.Slider([0, 1], default_value=sett.voice_vol, orientation='h', resolution=0.01, expand_x=True, pad = (50, 10), k='voice vol', disabled=True)]
+    ]
+
     settings = [
         [sg.Column(
                 [
-                    [sg.Frame('Theme', settings_theme)]
+                    [sg.Frame('Theme', settings_theme, font=(sett.font_b + ' bold', 16), expand_x=True)],
+                    [sg.Frame('Font', settings_fonts, font=(sett.font_b + ' bold', 16), expand_x=True)],
+                    [sg.Frame('Music', settings_music, font=(sett.font_b + ' bold', 16), expand_x=True)],
+                    [sg.Frame('Sound Effects', settings_sfx, font=(sett.font_b + ' bold', 16), expand_x=True)],
+                    [sg.Frame('Speech [WIP]', settings_voice, font=(sett.font_b + ' bold', 16), expand_x=True)],
                 ],
                 scrollable=True,
                 vertical_scroll_only=True,
@@ -111,13 +152,28 @@ def Layouts():
         [sg.Button('Save', k='save settings')]
     ]
 
-    layout = [
-        [sg.Text('CodeRandom3', font='Nunito 24 bold')],
+    credits = [
+        [sg.Text('My many thanks to the talented people who created the assets I used\n to make this game happen and add extra flare.', font=(sett.font_b, 20))],
+        [sg.Text('Music', font=(sett.font_a, 18))],
+        [sg.Text('LudoLoon Studio', font=(sett.font_b, 16)), sg.Button('url', k='cred1', font=(sett.font_b, 16))],
+        [sg.Text('Aekseer', font=(sett.font_b, 16)), sg.Button('url', k='cred2', font=(sett.font_b, 16))],
+        [sg.Text('Nathan Gibson', font=(sett.font_b, 16)), sg.Button('url', k='cred3', font=(sett.font_b, 16))],
+    ]
+
+    layout_left = [
         [
-            sg.Frame('M\ne\nn\nu', menu, title_location='wn', vertical_alignment='c', element_justification='center'),
-            sg.Frame('Pick Mode', start, expand_x=True, expand_y=True, visible=False, k='new1'),
-            sg.Frame('Pick Save', cont, expand_x=True, expand_y=True, visible=False, k='cont1'),
-            sg.Frame('Settings', settings, expand_x=True, expand_y=True, visible=False, k='settings1')
+            sg.Frame('M\ne\nn\nu', menu, title_location='wn', vertical_alignment='c', element_justification='center', font=(sett.font_b + ' bold', 16)),
+            sg.Frame('Pick Mode', start, expand_x=True, expand_y=True, visible=False, k='new1', font=(sett.font_b + ' bold', 16)),
+            sg.Frame('Pick Save', cont, expand_x=True, expand_y=True, visible=False, k='cont1', font=(sett.font_b + ' bold', 16)),
+            sg.Frame('Settings', settings, expand_x=True, expand_y=True, visible=False, k='settings1', font=(sett.font_b + ' bold', 16)),
+            sg.Frame('Credits', credits, expand_x=True, expand_y=True, visible=False, k='credits1', font=(sett.font_b + ' bold', 16))
+        ]
+    ]
+
+    layout = [
+        [
+            sg.Column(layout_left, expand_y=True, expand_x=True, k='side left'),
+            # sg.Column([[tab_group()]], expand_x=True, expand_y=True, visible=True, k='tabgroup1')
         ]
     ]
 
